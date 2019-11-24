dc03b0c381bb47ab1ed2f1232c95ded39ffade88
Muhammad Atif <44219381+muhammadatifsajjad@users.noreply.github.com>
2019-11-24 20:22:35 +1100

    Update 1_GenerateDatasetForSupervisedClassification.py

--- 1_GenerateDatasetForSupervisedClassification.py
+++ 1_GenerateDatasetForSupervisedClassification.py
@@ -9,8 +9,8 @@
                disease and other features as specified (could be medicines, 
                atclevel3code, atclevel4code, atclevel5code) from patient history 
                merged into a single data frame with one record per patient  
-     Validation: 5-fold cross-validation on 75% training data 
-     Final test score: Using held-out 25% test data
+     Validation: 5-fold cross-validation on 80% training data 
+     Final test score: Using held-out 20% test data
 """
 
 import os
@@ -44,9 +44,9 @@ feature_type = ['disease', 'medicine', 'atclevel3code', 'atclevel4code',
  This 'train' will then be merged with other features as they are built
 """
 try: 
-    # Open connection with MySQL database
-    sf_conn = sf.connect(user='MuhammadAtif@UniMel', password='Inbox@123',
-                         account='ti70021.ap-southeast-2')
+    # Open connection with Snowflake database
+    sf_conn = sf.connect(user='******', password='******',
+                         account='******')
 
     #sf_conn = engine.connect()
     
